104e4909a7329acf5dd9935a9408a0faee7682d4
adamjmcgrath <adam.mcgrath@auth0.com>
2023-02-07 09:46:21 -0400

    add middleware

---
+++ middleware.js
@@ -0,0 +1,10 @@
+import { NextResponse } from 'next/server';
+
+export function middleware(req) {
+  const cookie = req.headers.get('cookie');
+
+  return new NextResponse(
+      JSON.stringify({ cookie, length: cookie.length }),
+      { headers: { 'content-type': 'application/json' } }
+  )
+}
